3b7c572b7b8cfd430cd4e44dec61469850690db7
Alina <example@gmail.com>
2025-11-19 21:41:22 +0200

    ended form and started slider

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,38 @@
+import Swiper from 'swiper';
+import { Navigation, Pagination } from 'swiper/modules';
+
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
 import "/src/sass/style.scss";
+
+try {
+    new Swiper('.works__slider', {
+        modules: [Navigation, Pagination],
+        slidesPerView: 1,
+        speed: 300,
+        loop: true,
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+        breakpoints: {
+            1200: {
+                slidesPerView: 3,
+                spaceBetween: 5,
+            },
+            1920: {
+                slidesPerView: 3,
+                spaceBetween: 35,
+            }
+        }
+    });
+} catch (e) { }
+
+
+
